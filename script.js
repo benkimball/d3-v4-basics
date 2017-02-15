dfbf533b0d30228646133e40b944cff5c56953a1
Ben Kimball <github@benkimball.com>
2017-02-14 22:45:26 -0600

    Step 3: add readme, plot SVG rects

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-// associate a random number between 0 and 100 for each Tumblr post type
-// we'll use this as our data to display in a column chart
-// the value of the `data` variable will have the form:
-// [{category: 'foo', count: 23}, {category: 'bar', count: 31}]
+/*
+ * Data setup
+ *
+ * associate a random number between 0 and 100 for each Tumblr post type
+ * we'll use this as our data to display in a column chart
+ * the value of the `data` variable will have the form:
+ * [{category: 'foo', count: 23}, {category: 'bar', count: 31}]
+ */
+
 var data = rnd_hist([
   "video", "link", "photo", "audio", "chat", "quote", "answer", "text"
 ]);
 
-var category = d3
-  .select("body")
-  .selectAll(".category") // create (empty) selection
-  .data(data.sort(category_order)); // perform data join, returning 'update' selection
+/*
+ * Plotting
+ */
 
-category // from the 'update' selection,
+/* Create a 600x300 SVG element and append it to <body> with class `plot` */
+var svg = d3
+  .select("body")
+    .append("svg")
+      .attr("class", "plot")
+      .attr("width", 600)
+      .attr("height", 300);
+
+svg
+  .selectAll('.category') // create (empty) selection
+  .data(data.sort(category_order)) // perform data join, returning 'update' selection
   .enter() // retrieve 'enter' selection
-  .append('p') // for each item in the 'enter' selection, append a <p> element
+  .append('rect') // for each item in the 'enter' selection, append a <rect> SVG element
     .attr('class', 'category') // whose class attribute is 'category'
-    .html(function(d) { // and whose innerhtml is specified by this function
-      return "The '"+d.category+"' category contains "+d.count+" thing(s).";
-    });
+    .attr('x', 0) // and whose left side is at the left edge of the SVG
+    .attr('width', function(d) { return d.count; }) // whose width is exactly the count for the category
+    .attr('y', function(d, i) { return i * 20; }) // whose top edge is 20px down the page per item
+    .attr('height', 10); // and whose height is 10px
+
+
+/*
+ * Utility functions
+ */
 
 function rnd_hist(labels) {
   var hist = [], ct = 0;
