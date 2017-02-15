e7f83a34491a47595dc3645eff8c0022d120eef6
Ben Kimball <github@benkimball.com>
2017-02-14 23:43:33 -0600

    Step 8: Fix ordering bug, reorg code, accessors

--- README.md
+++ README.md
@@ -24,3 +24,17 @@ We've got a lot of magic numbers in our script. Each bar is a fixed height and p
 Step 6: Margins
 
 So far I've been ignoring the fact that this histogram is useless; it doesn't tell us which category is represented by which bar. There are a number of ways to display this information, but we'll use conventional axes. But before we jump in to the details of the D3 axis object, we need to address the lack of margins in our plot. Right now there's no space for an axis to live. We'll use Mike Bostock's margin convention, http://bl.ocks.org/mbostock/3019563.
+
+Step 7: Axes
+
+Our first goal is to draw a horizontal axis with appropriate tick marks to show the values. Our second goal is to print the name of each category to the left of its associated bar. You could draw all of these things yourself by doing a bunch of math and then appending SVG text and line elements, but that would be dumb. Use D3!
+
+Step 8: Troubleshooting
+
+Wait a minute. What happened to our sorting? Our new Y axis shows us that we're no longer sorting by category name. How can we be sure that our plot is using the right data? Well, let's start by adding tooltips to the bars to make sure that we don't have a mismatch between the axis labels and the bars.
+
+Nope, okay, so what changed? Well, we're now using the output of the y() function to determine the vertical position of each bar, and y() is an axis object. Let's check its API: https://github.com/d3/d3-axis. Nope, nothing about sorting or ordering there. But remember that we created the axis using an ordinal scale object, so let's check that: https://github.com/d3/d3-scale/blob/master/README.md#scaleOrdinal. Bingo! The description of the domain([values]) function says in part, "[i]f domain is specified, sets the domain to the specified array of values. The first element in domain will be mapped to the first element in the range, the second domain value to the second range value, and so on." And when we call axis.domain(), we're passing in an unsorted list of category names.
+
+So how do we fix it? First, for clarity, let's put all of our plotting code into a function and all of our data loading code into a different function. Now we just need to perform the data sort at the beginning of the plotting code.
+
+Finally, while we're cleaning things up, let's add an accessor functions for category, since we call that function repeatedly.
--- script.js
+++ script.js
@@ -7,77 +7,95 @@
  * [{category: "foo", count: 23}, {category: "bar", count: 31}]
  */
 
-var category_names = [
-  "video", "link", "photo", "audio", "chat", "quote", "answer", "text"
-];
-var data = rnd_hist(category_names);
-
-var canvas_width = 600,
-    canvas_height = 300;
-
-/* d3 "Conventional Margins": http://bl.ocks.org/mbostock/3019563 */
-var margin = {top: 20, right: 10, bottom: 20, left: 60};
-var width = canvas_width - margin.left - margin.right,
-    height = canvas_height - margin.top - margin.bottom;
-
-/*
- * Set up scales for the X and Y axes
- */
-var x = d3.scaleLinear()
-        .domain([0, 100])
-        .rangeRound([0, width]),
-    y = d3.scaleBand()
-        .domain(category_names)
-        .range([0, height])
-        .padding(0.1);
-
-/* Create the root SVG element */
-var svg = d3
-  .select("body")
-    .append("svg")
-      .attr("class", "plot")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-/* Plot a rectangle for each category */
-svg
-  .selectAll(".category") // create (empty) selection
-  .data(data.sort(category_order)) // perform data join, returning "update" selection
-  .enter() // retrieve "enter" selection
-  .append("rect") // for each item in the "enter" selection, append a <rect> SVG element
-    .attr("class", "category") // whose class attribute is "category"
-    .attr("x", 0.5) // and whose left side is at the left edge of the SVG
-    .attr("width", function(d) { return x(d.count); }) // whose width is exactly the count for the category
-    .attr("y", function(d, i) { return y(d.category); }) // whose top edge is 20px down the page per item
-    .attr("height", y.bandwidth()); // and whose height is 10px
-
-/* Plot the X and Y axes */
-svg.append("g")
-  .attr("class", "x axis")
-  .attr("transform", "translate(0, "+height+")")
-  .call(d3.axisBottom(x));
-
-svg.append("g")
-  .attr("class", "y axis")
-  .call(d3.axisLeft(y));
-
-/*
- * Utility functions
- */
-
-function rnd_hist(labels) {
-  var hist = [], ct = 0;
-  labels.forEach(function(label) {
-    ct = Math.floor(Math.random()*100);
-    hist.push({category:label, count:ct});
-  });
-  return(hist);
+function load_data() {
+  var category_names = [
+    "video", "link", "photo", "audio", "chat", "quote", "answer", "text"
+  ];
+  function rnd_hist(labels) {
+    var hist = [], ct = 0;
+    labels.forEach(function(label) {
+      ct = Math.floor(Math.random()*100);
+      hist.push({category:label, count:ct});
+    });
+    return(hist);
+  };
+  return(rnd_hist(category_names));
 };
 
-function category_order(a, b) {
-  if(a.category > b.category) return 1;
-  else if(a.category < b.category) return -1;
-  return 0;
-};
+/*
+ * Plot a histogram of category counts.
+ */
+function plot(data, dimensions) {
+  /* accessors */
+  function category(d) { return d.category; }
+  function count(d) { return d.count; }
+
+  /* comparators */
+  function category_order(a, b) {
+    if(a.category > b.category) return 1;
+    else if(a.category < b.category) return -1;
+    return 0;
+  };
+
+  data = data.sort(category_order);
+ 
+  /* d3 "Conventional Margins": http://bl.ocks.org/mbostock/3019563 */
+  var margin = {top: 20, right: 10, bottom: 20, left: 60};
+  var width = dimensions.width - margin.left - margin.right,
+      height = dimensions.height - margin.top - margin.bottom;
+
+  /* Create the root SVG element */
+  var svg = d3
+    .select("body")
+      .append("svg")
+        .attr("class", "plot")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  /*
+   * Set up scales for the X and Y axes
+   */
+  var x = d3.scaleLinear()
+          .domain([0, 100])
+          .rangeRound([0, width]),
+      y = d3.scaleBand()
+          .domain(data.map(category))
+          .range([0, height])
+          .padding(0.1);
+
+  /*
+   * Set up SVG axes for the plot
+   */
+  var xAxis = d3.axisBottom(x),
+      yAxis = d3.axisLeft(y);
+
+  /*
+   * Draw the axes
+   */
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0,"+height+")")
+      .call(xAxis);
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+
+  /* Plot a rectangle for each category */
+  svg
+    .selectAll(".category") // create (empty) selection
+    .data(data.sort(category_order)) // perform data join, returning "update" selection
+    .enter() // retrieve "enter" selection
+    .append("rect") // for each item in the "enter" selection, append a <rect> SVG element
+      .attr("class", "category") // whose class attribute is "category"
+      .attr("x", 0.5) // and whose left side is at the left edge of the SVG
+      .attr("width", function(d) { return x(d.count); }) // whose width is exactly the count for the category
+      .attr("y", function(d, i) { return y(d.category); }) // whose top edge is 20px down the page per item
+      .attr("height", y.bandwidth()); // and whose height is 10px
+
+
+}
+
+plot(load_data(), {width: 600, height: 300});
+
